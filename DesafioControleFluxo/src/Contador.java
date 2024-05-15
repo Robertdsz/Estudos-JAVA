d247fb407082c945ae06b7659c93c9c46762ad54
Robert Zica <randondesigner19@gmail.com>
2024-05-15 14:18:40 -0300

    adicionando desafio de controle de fluxo

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -6,11 +6,35 @@ public class Contador {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Digite o primeiro número:");
+
+		System.out.print("Digite o primeiro número: ");
 		int parametroUm = sc.nextInt();
-		System.out.print("Digite o segundo número:");
+		System.out.print("Digite o segundo número: ");
 		int parametroDois = sc.nextInt();
+		sc.close();
+		try
+
+		{
+			contar(parametroUm, parametroDois);
+		} catch (ParametrosInvalidosException exception) {
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
+		}
 	}
 
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		if (parametroDois < parametroUm) {
+			throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro!");
+		}
+
+		int contagem = parametroDois - parametroUm;
+		for (int i = 1; i<= contagem; i++) {
+			System.out.println("Imprimindo o número " + i);
+		}
+	}
+}
+
+class ParametrosInvalidosException extends Exception {
+	public ParametrosInvalidosException(String message) {
+		super(message);
+	}
 }
