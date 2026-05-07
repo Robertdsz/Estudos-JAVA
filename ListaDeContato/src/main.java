4d85b10616889490f69b24d7e94f02d3df29bd82
randontime <randondesigner19@gmail.com>
2026-05-06 23:55:36 -0300

    Adicionando interação do usuário com o console

--- ListaDeContato/src/main.java
+++ ListaDeContato/src/main.java
@@ -1,5 +1,6 @@
 package src;
 import java.util.List;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
@@ -7,11 +8,33 @@ public class main {
         ContatoService service = new ContatoService();
         List<Contato> lista = service.listarContatos();
 
-        service.adicionarContato(new Contato("Robert", "987654321", "robert@email.com"));
-        service.adicionarContato(new Contato("Ana", "707070987", "ana@email.com"));
+        Scanner sc = new Scanner(System.in);
+        int opcao;
 
-        for (Contato contato : lista){
-            System.out.println(contato.getNome() + " - " + contato.getTelefone());
+        do {
+            System.out.println("=== Agenda de Contatos ===");
+            System.out.println("[1] Adicionar");
+            System.out.println("[2] Listar");
+            System.out.println("[3] Buscar por nome");
+            System.out.println("[4] Atualizar telefone");
+            System.out.println("[5] Remover");
+            System.out.println("[0] Sair");
+            System.out.println("Escolha: ");
+
+            opcao = sc.nextInt();
+
+            switch (opcao){
+                case 1:
+                    System.out.println("Nome: ");
+                    String nome = sc.nextLine();
+                    System.out.println("Telefone: ");
+                    String telefone = sc.nextLine();
+                    System.out.println("Email: ");
+                    String email = sc.nextLine();
+                    service.adicionarContato(new Contato(nome, telefone, email));
+                    System.out.println("Contato adicionado com sucesso!");
+                    break;
+            }
         }
     }
 }
